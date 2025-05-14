8fa96efff548d793896dedd84ddb7ccc12d7ad19
Antonchyk Denis <tenirey11@gmail.com>
2025-05-14 18:45:19 +0300

    -created auth-service.
    Also created docker image and connected to auth database

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,30 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+
+
+}
